dedb90ae46f4a839684c5f3198f12b1dcd54b0bc
mrjasonwalton00 <jpwalton00@gmail.com>
2022-11-20 19:54:41 -0500

    version-2

--- toDoList/urls.py
+++ toDoList/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from . import views
+from .views import TaskList, TaskDetail, TaskCreate, DeleteView, TaskUpdate
 
 
 urlpatterns = [
-    path('', views.toDoList, name = "toDoList"),
-    path('page2/', views.page2, name = "page2"),
-    path('page1/', views.page1, name = "page1"),
-    path('page3/', views.page3, name = "page3"),
+    path('', TaskList.as_view(), name = "tasks"),
+    path('dummy/<int:pk>/', TaskDetail.as_view(), name = 'task'),
+    path('task-create/', TaskCreate.as_view(), name = 'task-create'),
+    path('task-delete/<int:pk>/', DeleteView.as_view(), name = 'task-delete'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name = 'task-update'),
+
+    
+    #path('add/', views.add, name = "add"),
+    #path('update/', views.update, name = "update"),
+    
+    
+    #path('dummy/', TaskList.as_view(), name = 'tasks'),
+    #path('index/', views.index, name = "index"),
 ]
--- toDoList/views.py
+++ toDoList/views.py
@@ -1,14 +1,78 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
 
-def toDoList(request):
-    return render(request, 'toDoList/page1.html')
 
-def page2(request):
-    return render(request, 'toDoList/page2.html')
 
-def page1(request):
-    return render(request, 'toDoList/page1.html')
+#shows list of tasks on landing page
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'toDoList/index.html'
 
-def page3(request):
-    return render(request, 'toDoList/page3.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
+
+        context['search_input'] = search_input
+        return context
+
+
+
+
+    
+#shows specific task on dummy page
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'toDoList/dummy.html'
+
+#adds a task on add page
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    template_name = 'toDoList/add.html'
+
+#delete task on landing page
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    template_name = 'toDoList/update.html'
+    
+
+#def index(request):
+    #return render(request, 'toDoList/index.html')
+
+
+#def toDoList(request):
+    #return render(request, 'toDoList/index.html')
+
+#def dummy(request):
+    #return render(request, 'toDoList/dummy.html')
+
+
+
+
+
+#def add(request):
+    #return render(request, 'toDoList/add.html')
+
+
+
+#def update(request):
+    #return render(request, 'toDoList/update.html')
+
+
